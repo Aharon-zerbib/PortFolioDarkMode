2da6db9b0a519e7ac0cccb8ad673a0b0f921b216
Aharon-zerbib <amzyt770@gmail.com>
2025-03-26 21:23:15 +0100

    feat: -rectification des lien -rectification des couleur

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -2,12 +2,12 @@ const Connect = () => {
     return  (
     <footer className="mt-24 w-full max-w-3xl mx-auto">
         <h2 className="text-lg font-medium">Connect</h2>
-        <p className="mt-2 text-gray-400">
+        <p className="mt-2 text-gray-500 dark:text-gray-400">
           Feel free to contact me at <a className="underline dark:text-zinc-300" >Amzyt770@gmail.com</a>
         </p>
         <div className="mt-4  space-x-6 ">
-          <a href="#" className="hover:text-gray-400">Github</a>
-          <a href="#" className="hover:text-gray-400">LinkedIn</a>
+          <a href="https://github.com/Aharon-zerbib" className="hover:text-gray-400">Github</a>
+          <a href="https://www.linkedin.com/in/aharon-zerbib-911bb6276/" className="hover:text-gray-400">LinkedIn</a>
          
         </div>
         <p className="mt-20 text-gray-500">Port Folio Ronron</p>
